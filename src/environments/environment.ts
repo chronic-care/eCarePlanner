4cb87cce8c7cc443e421cd2fb2868cfbb6480d79
svalluripalli <sai@digitalhie.com>
2023-07-10 22:11:56 -0400

    Added logging to Goal Service..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  mccapiUrl:  window['env']['apiServer'] || 'http://localhost:8081'
-
+  mccapiUrl:  window['env']['apiServer'] || 'http://localhost:8081',
+  logEndpointUri:  window['env']['logEndpointUri'] || 'http://localhost:8085',
+  logApiKey:  window['env']['logApiKey'] || 'dd9761de-dc5e-4132-a159-51421fc43e07'
 };
